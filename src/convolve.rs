b902e795170fea19f686e7d63f8681feb467bc66
clorc <kaaddi@student.ethz.ch>
2023-04-15 00:34:23 +0200

    WIP generate_2d_toeplitz

--- src/convolve.rs
+++ src/convolve.rs
@@ -1,26 +1,29 @@
-use nalgebra::{base::Matrix, Dyn, Dim, Const};
+use nalgebra::{base, Dyn, Dim, Const};
+type DynamicMatrix = base::Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>>;
+type DynamicVector = base::Matrix<i32, Dyn, Const<1>, nalgebra::VecStorage<i32, Dyn, Const<1>>>;
 
-
-pub fn convolve<S>(image: Matrix<i32, Dyn, Dyn, S>, filter: Matrix<i32, Dyn, Dyn, S>)
-where S: nalgebra::Storage<i32, Dyn, Dyn>
+pub fn convolve(image: DynamicMatrix, filter: DynamicMatrix)
 {
-    let height:Dyn = Dyn(image.nrows() + filter.nrows() - 1);
-    let width:Dyn = Dyn(image.ncols() + filter.ncols() - 1);
+    let height = Dyn(image.nrows() + filter.nrows() - 1);
+    let width = Dyn(image.ncols() + filter.ncols() - 1);
 
-    let zero_padded: Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>> = zero_pad(filter, height, width);
-    // let doubly_blocked_toeplitz: Vec<Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>>> = generate_2d_toeplitz(zero_padded);
+    let zero_padded: DynamicMatrix = zero_pad(filter, height, width);
+    generate_2d_toeplitz(zero_padded, height.value(), width.value(), image.nrows()*image.ncols());
 }
 
-fn generate_1d_toeplitz(vector: Matrix<i32, Dyn, Const<1>, nalgebra::VecStorage<i32, Dyn, Const<1>>>, ncols: usize)
--> Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>>
-{
-    let mut toeplitz: Matrix<i32,Dyn,Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>> = Matrix::zeros_generic(Dyn(vector.nrows()), Dyn(ncols));
-    let mut sliding_vector: Matrix<i32, Dyn, Const<1>, nalgebra::VecStorage<i32, Dyn, Const<1>>> = Matrix::zeros_generic(Dyn(vector.nrows()), Const);
+fn generate_1d_toeplitz(vector: DynamicVector, ncols: usize) -> DynamicMatrix {
+    // initializing our toeplitz with the correct dimensions
+    let mut toeplitz: DynamicMatrix = base::Matrix::zeros_generic(Dyn(vector.nrows()), Dyn(ncols));
+
+    // copying the vector we're computing the toeplitz for as mutable vector
+    let mut sliding_vector: DynamicVector = base::Matrix::zeros_generic(Dyn(vector.nrows()), Const);
     sliding_vector.copy_from(&vector);
 
     
     for i in 0..=ncols-1 {
         toeplitz.set_column(i, &sliding_vector);
+
+        // sliding down the entries of the sliding vector
         sliding_vector = sliding_vector.insert_row(0, 0);
         sliding_vector = sliding_vector.remove_row(vector.nrows());
     }
@@ -28,18 +31,29 @@ fn generate_1d_toeplitz(vector: Matrix<i32, Dyn, Const<1>, nalgebra::VecStorage<
     toeplitz
 }
 
-fn _generate_2d_toeplitz<S>(_matrix: Matrix<i32, Dyn, Dyn, S>) {
+fn generate_2d_toeplitz(matrix: DynamicMatrix, ncols: usize, nrows:usize, image_vector_rows: usize) {
+    let mut toeplitz_vector: Vec<DynamicMatrix> = Vec::new();
 
+    // computing the toeplitz of each row of the matrix
+    for i in 0..=matrix.nrows()-1 {
+        toeplitz_vector.push(generate_1d_toeplitz(matrix.row(i).transpose(), ncols));
+    }
+
+    // initializing the block matrix
+    let mut block_toeplitz: DynamicMatrix = base::Matrix::zeros_generic(Dyn(toeplitz_vector.len()*nrows), Dyn(image_vector_rows));
+
+    
 }
 
-fn zero_pad<S>(matrix: Matrix<i32, Dyn, Dyn, S>, height: Dyn, width: Dyn)
--> Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>>
-where S: nalgebra::Storage<i32, Dyn, Dyn>
+// resizing a matrix to new "height x width" size, with the old entries in the bottom right and zeros everywhere else
+// height >= matrix.nrows and width >= matrix.ncols 
+fn zero_pad(matrix: DynamicMatrix, height: Dyn, width: Dyn)
+-> DynamicMatrix
 {
     let matrix_rows = matrix.nrows();
     let matrix_cols = matrix.ncols();
 
-    let padded: Matrix<i32, Dyn, Dyn, nalgebra::VecStorage<i32, Dyn, Dyn>> = matrix.insert_rows(0, height.value()-matrix_rows, 0);
+    let padded: DynamicMatrix = matrix.insert_rows(0, height.value()-matrix_rows, 0);
     padded.insert_columns(matrix_cols, width.value()-matrix_rows, 0)
 }
 
